c344415ba103b2322eca86ef7492d0ef2c10d3a3
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-08-20 14:34:40 +0100

    Dashboard content woudnt load through App call, originally had element=Dash, changed element to render

--- react/src/App.js
+++ react/src/App.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import NavBar from './components/NavBar';
 
 
 const App = () => {
     return (
-        <>
-        <Switch>
-            <Route path="/" element={<Dashboard />} />
-            {/* <Route path="/stocks" element={<Stocks />} /> */}
-        </Switch>
-        </>
+    <>
+        <div style={{"height": "100%"}}>
+            <NavBar />
+            
+            <Switch>
+                <Route path="/" render={() => {
+                    return <Dashboard />;
+                }} />
+                {/* <Route path="/discover" element={<Discover />} /> */}
+            </Switch>
+        </div>
+    </>
+
     );
 };
+
+// // Debugging as Dashboard content doesn't load when called through App
+// const App = () => {
+//     return (
+//         <div>
+//             <NavBar />
+//             <h2>Debug: App Component Rendered</h2>
+//             <Switch>
+//                 <Route exact path="/" render={() => {
+//                     console.log("/ route matched");
+//                     return <Dashboard />;
+//                 }} />
+//                 <Route path="*" render={() => <div>No route matched</div>} />
+//             </Switch>
+//         </div>
+//     );
+// };
 
 export default App;
