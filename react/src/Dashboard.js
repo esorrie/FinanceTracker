6285920efec0de33415253f6ed27ac9be0b3465d
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-08-20 15:28:25 +0100

    added display: none to inactive tab content

--- react/src/Dashboard.js
+++ react/src/Dashboard.js
@@ -2,56 +2,67 @@ import React, { useState, useEffect } from 'react';
 import './Dashboard.css';
 
 function Dashboard() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetch('/dashboard')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+    useEffect(() => {
+        fetch('/dashboard')
+        .then(response => {
+            if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => setData(data))
+        .catch(error => {
+            console.error('Error:', error);
+            setError(error.message);
+        });
+    }, []);
+
+        // Dynamic tab rendering 
+        const [toggleState, setToggleState] = useState(1);
+
+        const toggleTab = (index) => {
+        setToggleState(index);
+        console.log(index);
         }
-        return response.json();
-      })
-      .then(data => setData(data))
-      .catch(error => {
-        console.error('Error:', error);
-        setError(error.message);
-      });
-  }, []);
-
-    // Dynamic tab rendering 
-    const [toggleState, setToggleState] = useState(1);
-
-    const toggleTab = (index) => {
-      setToggleState(index);
-      console.log(index);
-    }
-  return (
-    <>
-      {/* <div>
-        <h1>Data from Flask:</h1>
-        {error ? <p>Error: {error}</p> : 
-          data ? <p>{data.message}</p> : 
-          <p>Loading...</p>}
-      </div> */}
-      <div>
-        <div className="dashFirst" >
-          <div className="dashFirstTabs" >
-            <div className= {toggleState === 1 ? "dashFirstStocks cursorDefault active-tab" : "dashFirstStocks" } onClick={() => toggleTab(1) } >
-              Top Stocks
-            </div>
-            <div className= {toggleState === 2 ? "dashFirstIndexes cursorDefault active-tab" : "dashFirstIndexes" } onClick={() => toggleTab(2) } >
-              Indexes
-            </div>
-            <div className= {toggleState === 3 ? "dashFirstCurrency cursorDefault active-tab" : "dashFirstCurrency" } onClick={() => toggleTab(3) } >
-              Currencies
-            </div>
-          </div>
+    return (
+        <>
+        <div>
+            
         </div>
-      </div>
-    </>
-  );
+        <div>
+            <div className="dashFirst" >
+            <div className="dashFirstTabs" >
+                <div className= {toggleState === 1 ? "dashFirstStocks cursorDefault active-tab" : "dashFirstStocks" } onClick={() => toggleTab(1) } >
+                Top Stocks
+                </div>
+                <div className= {toggleState === 2 ? "dashFirstIndexes cursorDefault active-tab" : "dashFirstIndexes" } onClick={() => toggleTab(2) } >
+                Indexes
+                </div>
+                <div className= {toggleState === 3 ? "dashFirstCurrency cursorDefault active-tab" : "dashFirstCurrency" } onClick={() => toggleTab(3) } >
+                    Currencies
+                </div>
+            </div>
+            <div className="tabContent">
+                <div className= { toggleState === 1 ? "content activeContent" : "content" } >
+                    <h1>Data from Flask:</h1>
+                        {error ? <p>Error: {error}</p> : 
+                        data ? <p>{data.message}</p> : 
+                        <p>Loading...</p>}
+                </div>
+                <div className= { toggleState === 2 ? "content activeContent" : "content" } >
+                    <h1> Indexes </h1>
+                </div>
+                <div className= { toggleState === 3 ? "content activeContent" : "content" } >
+                    <h1> Currency </h1> 
+                </div>
+            </div>
+            </div>
+        </div>
+        </>
+    );
 }
 
 export default Dashboard;
