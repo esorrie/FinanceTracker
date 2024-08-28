66d657a0f67cbe9eeec2f6b5c53883efefdb833f
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-08-28 15:29:03 +0100

    displaying Index data on dashboard, updates hourly

--- react/src/Dashboard.js
+++ react/src/Dashboard.js
@@ -1,29 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import ActiveTab from './components/activeTabs';
-import DashData from './components/dashData';
+import DashIndices from './components/dashIndices';
+import axios from 'axios';
 import './Dashboard.css';
 
 function Dashboard() {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch('/dashboard')
-        .then(response => {
-            if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => setData(data))
-        .catch(error => {
-            console.error('Error:', error);
-            setError(error.message);
-        });
-    }, []);
-
     // Dynamic tab rendering 
     const [activeTab, setActiveTab] = useState(1); //change state back to 0
+    const [activeStockTab, setActiveStockTab] = useState(0);
+    const [indicesData, setIndicesData] = useState([]);
+
 
     const tabs = [
         { label: 'Top Stocks' },
@@ -31,71 +17,34 @@ function Dashboard() {
         { label: 'Currencies' }
     ];
 
-    const indicesData = [
-        {
-            title: "S&P 500 (GSPC)",
-            currency: "USD",
-            data: {
-                "open ": 25000,
-                "prev close ": 25000,
-                "day range ": 25000,
-                "52w range ": 25000,
-                "volume ": 25000,
-                "avg. volume": 30000
-            }
-        },
-        {
-            title: "NASDAQ (IXIC)",
-            currency: "USD",
-            data: {
-                "open": 25000,
-                "close": 25000,
-                "day's range": 25000,
-                "52w range": 25000,
-                "prev close": 25000,
-                "volume": 25000
-            }
-        },
-        {
-            title: "FTSE 100 (FTSE)",
-            currency: "GBP",
-            data: {
-                "open": 25000,
-                "close": 25000,
-                "day's range": 25000,
-                "52w range": 25000,
-                "prev close": 25000,
-                "volume": 25000
-            }
-        },
-        {
-            title: "RUSSEL 2000 (RUT)",
-            currency: "USD",
-            data: {
-                "open": 25000,
-                "close": 25000,
-                "day's range": 25000,
-                "52w range": 25000,
-                "prev close": 25000,
-                "volume": 25000
-            }
-        },
-    ]
+    useEffect(() => {
+        fetchIndicesData();
+    }, []);
 
-    const currencyData = [
-        {
-            title: "GBP/USD",
-            currency: "GBP",
-            data: {
-                "open": 25000,
-                "close": 25000,
-                "day's range": 25000,
-                "52w range": 25000,
-                "prev close": 25000,
-                "volume": 25000
-            }
-        },
-    ]
+    const fetchIndicesData = async () => {
+        try {
+            // console.log('fetching data');
+            const response = await axios.get('/dashboard')
+            // console.log('response received:', response);
+            setIndicesData(response.data)
+            // console.log('indices data set:', response.data);
+        } catch (error) {
+            console.error('Error fetching indices data:', error);
+        }
+    };
+
+    const indicesCurrency = (ticker) => {
+        switch (ticker) {
+            case '^GSPC':
+            case '^IXIC':
+            case '^RUT':
+                return 'USD';
+            case '^FTSE':
+                return 'GBP';
+            default:
+                return '';
+        }
+    };
 
     const renderMainTabContent = () => {
         switch(activeTab) {
@@ -118,12 +67,10 @@ function Dashboard() {
                                 <div className='activeContent graphsLayout'>
                                     
                                     {indicesData.map((index, i) => (
-                                        <DashData
+                                        <DashIndices
                                             key={i}
-                                            title={index.title}
-                                            ticker={index.ticker}
-                                            currency={index.currency}
-                                            data={index.data}
+                                            index={index}
+                                            currency={indicesCurrency(index.ticker)}
                                         />
                                     ))}
                                 </div>
@@ -134,7 +81,7 @@ function Dashboard() {
                             <div className="activeContentContainer" >
                                 <div className='activeContent graphsLayout'>
                                     
-                                    {currencyData.map((index, i) => (
+                                    {/* {currencyData.map((index, i) => (
                                         <DashData
                                             key={i}
                                             title={index.title}
@@ -142,14 +89,13 @@ function Dashboard() {
                                             currency={index.currency}
                                             data={index.data}
                                         />
-                                    ))}
+                                    ))} */}
                                 </div>
                             </div>
                         </>
         }
     };
     
-    const [activeStockTab, setActiveStockTab] = useState(0);
     
     const stockTabs = [
         { label: 'Big Tech' },
---
+++ react/src/components/dashIndices.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const DashIndices = ({ index, currency }) => {
+    const formatValue = (key, value) => {
+        if (key === "volume") {
+            return value.toLocaleString();
+        } else if (key === "changePercentage") {
+            return `${value.toFixed(2)}%`;
+        } else if (typeof value === 'number' ) {
+            return value.toFixed(2);
+        }
+        return value;
+    };
+
+    const dataOrder = [
+        'price', 'open', 'prev_close', 'dayRange', 
+        'yearRange', 'changePercentage', 'volume',
+    ];
+
+    const labelMap = {
+        price: 'Price',
+        open: 'Open',
+        prev_close: 'Prev Close',
+        dayRange: 'Day Range',
+        yearRange: '52 Week Range',
+        volume: 'Volume',
+        changePercentage: 'Change %'
+    };
+
+    return (
+        <>
+            <div className="graphLayout">
+                <div className="graphTitle">
+                    {index.name} ({index.ticker}) - {currency}
+                </div>
+
+                <div className="tableContent">
+                    {dataOrder.map((key) => (
+                        <div key={key} className="tr">    
+                            <div className="dataLabel"> {labelMap[key]} </div>
+                            <div className="dataValue"> {formatValue(key, index[key])} </div>
+                        </div>
+                    ))} 
+                </div>
+            </div> 
+        </>
+    );
+};
+
+export default DashIndices;
