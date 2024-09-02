d10c5f454940e5ccbe60105c44b441ab9c060d8b
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-09-02 16:02:01 +0100

    adjusted db column constraints

--- flask/models/etfs.py
+++ flask/models/etfs.py
@@ -3,7 +3,7 @@ from extensions import db
 class Etfs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ticker = db.Column(db.String(80), unique=True, nullable=False)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     exchange = db.Column(db.String(80), nullable=False)
     exchangeShort = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=True)
