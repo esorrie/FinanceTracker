92b4032cc8e4702e2f96bc30c06d61ba27d7474b
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-09-03 16:42:59 +0100

    added function to update price separately from other data

--- flask/services/stocks_service.py
+++ flask/services/stocks_service.py
@@ -80,3 +80,44 @@ def get_stock_data():
             error_message = f"An error occurred: {str(e)}"
             print(error_message)
             return False, error_message
+        
+        
+def update_stocks_price():
+    exchanges = ['NASDAQ', 'NYSE', 'LSE']
+    skipped_stocks = 0
+    
+    for exchange in exchanges:
+        url = f'{current_app.config["FMP_API"]}/symbol/{exchange}?apikey={current_app.config["FMP_API_KEY"]}'
+        
+        try: 
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            
+            for stock in data:
+                
+                if any(stock.get(field) is None for field in [
+                    'price', 'changesPercentage', 'change', 'dayLow', 'dayHigh',
+                    'yearHigh', 'yearLow', 'marketCap', 'exchange', 'volume',
+                    'avgVolume', 'open', 'previousClose', 'eps', 'pe',
+                    'earningsAnnouncement', 'sharesOutstanding', 'symbol', 'name'
+                ]):
+                    skipped_stocks += 1
+                    continue
+                
+                stock_data = {
+                    'price': stock['price']
+                }
+                
+                existing_stock = Stocks.query.filter_by(ticker=stock['symbol']).first()
+                
+                if existing_stock:
+                    for key, value in stock_data.items():
+                        setattr(existing_stock, key, value)
+                        
+            db.session.commit()
+            message = f"stock data fetched and stored successfully for price update at {datetime.now()}"
+            print(message)
+            return True, message
+        except Exception as e:
+            return False, str(e)
