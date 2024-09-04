da9381a3f87d60ef9824cae1969d46071fa73a04
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-09-04 17:13:17 +0100

    formatting MktCap to be readable

--- react/src/components/allStocks.js
+++ react/src/components/allStocks.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 const AllStocks = ({ stock }) => {
+    const formatMktcap = (value) => {
+        if (value >= 1e12) {
+            return `${(value / 1e12).toFixed(1)}T`;
+        } else if (value >= 1e9) {
+            return `${(value / 1e9).toFixed(1)}B`;
+        } else if (value >= 1e6) {
+            return `${(value / 1e6).toFixed(1)}M`;
+        } else {
+            return value.toLocaleString();
+        }
+    };
+
     const formatValue = (key, value) => {
-        if (key === "volume") {
+        if (key === "volume" || "avg_volume" ) {
             return value.toLocaleString();
         } else if (key === "changePercentage") {
             return `${value.toFixed(2)}%`;
+        } else if (key === 'marketCap') {
+            return formatMktcap(value);
         } else if (typeof value === 'number' ) {
             return value.toFixed(2);
-        }
+        } 
         return value;
     };
 
@@ -33,15 +47,15 @@ const AllStocks = ({ stock }) => {
 
     return  <>
                 <div className="graphLayout">
-                    <div className="graphTitle">
+                    <div className="stockTitle">
                         {stock.name} ({stock.ticker}) - {stock.currency}
                     </div>
 
-                    <div className="tableContent">
+                    <div className="tableStockContent">
                         {dataOrder.map((key) => (
-                            <div key={key} className="tr">
-                                <div className="dataLabel"> {labelMap[key]} </div>
-                                <div className="dataValue"> {formatValue(key, stock[key])} </div>
+                            <div key={key} className="trStocks">
+                                <div className="stockLabel"> {labelMap[key]} </div>
+                                <div className="stockValue"> {formatValue(key, stock[key])} </div>
                             </div>
                         ))}
                     </div>
