160e6b86311ada7fc98b066e9a02f6aaedd0485b
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-09-05 18:05:09 +0100

    increased stock name string limit

--- flask/models/stock.py
+++ flask/models/stock.py
@@ -2,7 +2,7 @@ from extensions import db
 
 class Stocks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
     ticker = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float,  nullable=False)
     changePercentage = db.Column(db.Float,  nullable=False)
